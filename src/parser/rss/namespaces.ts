078c8746212039f59ad6b586b8e7a0ef23118fbe
Ahmed T. Ali <ah.tajelsir@gmail.com>
2019-02-25 20:28:32 +0300

    refactor(rss): namespaces don't have to be a Map

--- src/parser/rss/namespaces.ts
+++ src/parser/rss/namespaces.ts
@@ -3,11 +3,11 @@
  *
  * Lookup by URI
  */
-const namespaces = new Map([
-	['http://www.w3.org/2005/Atom', 'atom'],
-	['http://purl.org/rss/1.0/modules/content/', 'content']
-]);
+const namespaces = {
+	'http://www.w3.org/2005/Atom': 'atom',
+	'http://purl.org/rss/1.0/modules/content/': 'content'
+};
 
 export function nsLookup(uri: string) {
-	return namespaces.get(uri) || '';
+	return namespaces[uri] || '';
 }
