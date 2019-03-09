b12a6289875e32de78e8f1dcd4b097de56f1824b
Ahmed T. Ali <ah.tajelsir@gmail.com>
2019-03-09 19:38:10 +0300

    build: generate declarations file

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
-import typescript from 'rollup-plugin-typescript2';
+import typescript from '@wessberg/rollup-plugin-ts';
 
 // Ours
 import pkg from './package.json';
