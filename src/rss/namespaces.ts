26746574c02aecee3150d4e7225f737081cb9923
Ahmed T. Ali <ah.tajelsir@gmail.com>
2019-07-10 09:27:33 +0300

    refactor(rss): add more namespaces

--- src/rss/namespaces.ts
+++ src/rss/namespaces.ts
@@ -1,11 +1,20 @@
 /*
- * Known namespaces
+ * Default namespaces.
+ *
+ * Adopted from the `feedparser` package.
  *
  * Lookup by URI
  */
 const namespaces = {
 	'http://www.w3.org/2005/Atom': 'atom',
-	'http://purl.org/rss/1.0/modules/content/': 'content'
+	'http://purl.org/rss/1.0/modules/content/': 'content',
+	'http://purl.org/dc/elements/1.1/': 'dc',
+	'http://purl.org/dc/elements/1.0/': 'dc',
+	'http://www.itunes.com/dtds/podcast-1.0.dtd': 'itunes',
+	'http://search.yahoo.com/mrss/': 'media',
+	'http://search.yahoo.com/mrss': 'media', // commonly-used but wrong
+	'http://www.w3.org/1999/xhtml': 'xhtml',
+	'http://www.w3.org/XML/1998/namespace': 'xml'
 };
 
 export function nsLookup(uri: string) {
